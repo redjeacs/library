0412900a3be964555a995df4dcd9fdedd4e76535
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-07-03 15:41:29 +0800

    function to add book to bookshelf
    instantiate the constructor in addBookToLibrary by
    calling new Book in bookObj varaible and push it
    into myLibrary array. createBookContainer function
    uses createElement to add a new div.book and for
    each property in the new Book add another div and
    alter its text content with property value.
    then usesdisplayBook funciton to append the newly
    created container to the bookshelf in html (grid).
    style the bookshelf and book container using css.

---
+++ script.js
@@ -0,0 +1,39 @@
+const myLibrary = [];
+const bookshelf = document.querySelector('.bookshelf');
+
+function Book(title, author, pages, isRead) {
+  if(!new.target) {
+    throw Error('must use the "new" operator to call the constructor')
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.id = crypto.randomUUID();
+};
+
+function addBookToLibrary(title, author, pages, isRead) {
+  let bookObj = new Book(title, author, pages, isRead);
+  myLibrary.push(bookObj);
+  displayBook(createBookContainter(bookObj));
+};
+
+function displayBook(book) {
+  bookshelf.appendChild(book);
+}
+
+function createBookContainter(bookObj) {
+  const book = document.createElement('div')
+  book.className = 'book';
+  book.id = bookObj.id
+  for(prop in bookObj) {
+    const txt = document.createElement('div');
+    txt.className = prop;
+    txt.textContent = bookObj[prop];
+    if(bookObj[prop] === 'Read') {
+      txt.classList.add('is-read');
+    }
+    book.appendChild(txt);
+  }
+  return book;
+}
