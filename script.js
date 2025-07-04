2adc118443db343bc005e75715d85887385e18c8
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-07-04 14:29:48 +0800

    submit button event listener
    event listener for submit button to call the
    addBookToLibrary function with the value of form
    submissions. add an if else check for isRead
    checkbox to correctly out put read or not read.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const myLibrary = [];
 const bookshelf = document.querySelector('.bookshelf');
 const addBtn = document.querySelector('.add-btn');
+const newBookDialog = document.querySelector('#new-book-dialog');
+const submitBtn = document.querySelector('#submit-btn');
 
 function Book(title, author, pages, isRead) {
   if(!new.target) {
-    throw Error('must use the "new" operator to call the constructor')
-  }
+    throw Error('must use the "new" operator to call the constructor');
+  };
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -21,20 +23,39 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function displayBook(book) {
   bookshelf.appendChild(book);
-}
+};
 
 function createBookContainter(bookObj) {
-  const book = document.createElement('div')
+  const book = document.createElement('div');
   book.className = 'book';
-  book.id = bookObj.id
+  book.id = bookObj.id;
   for(prop in bookObj) {
     const txt = document.createElement('div');
     txt.className = prop;
     txt.textContent = bookObj[prop];
     if(bookObj[prop] === 'Read') {
       txt.classList.add('is-read');
-    }
+    };
     book.appendChild(txt);
-  }
+  };
   return book;
-}
+};
+
+addBtn.addEventListener('click', () => {
+  newBookDialog.showModal();
+});
+
+submitBtn.addEventListener('click', () => {
+  const title = document.querySelector('#title-form').value;
+  const author = document.querySelector('#author-form').value;
+  const pages = document.querySelector('#pages-form').value;
+  const isRead = document.querySelector('#isRead-form');
+  let read = '';
+  if(isRead.checked) {
+    read = 'Read';
+  }
+  else {
+    read = 'Not read';
+  }
+  addBookToLibrary(title, author, pages, read);
+});
